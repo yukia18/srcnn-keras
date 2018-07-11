c161d0ad52ccc7861a3c8fa4b353413c01ec0cfe
yukia18 <abe@ailab.ics.keio.ac.jp>
2018-07-11 15:27:14 +0900

    tmp

--- utils.py
+++ utils.py
@@ -7,15 +7,11 @@ def load_train(image_size=33, label_size=21, stride=21):
     dirname = './train'
     dir_list = os.listdir(dirname)
     images = [cv2.cvtColor(cv2.imread(os.path.join(dirname,img)),cv2.COLOR_BGR2GRAY) for img in dir_list]
-    
-    # crop images
     images = [img[0:img.shape[0]-np.mod(img.shape[0],3),0:img.shape[1]-np.mod(img.shape[1],3)] for img in images]
 
-    # copy images for train and label
     trains = images.copy()
     labels = images.copy()
     
-    # make train images worse
     trains = [cv2.resize(img, None, fx=1/3., fy=1/3.) for img in trains]
     trains = [cv2.resize(img, None, fx=3/1., fy=3/1.) for img in trains]
 
@@ -39,33 +35,40 @@ def load_train(image_size=33, label_size=21, stride=21):
     return sub_trains, sub_labels
 
 def load_test():
-    '''
-    image shape is 256 * 256
-    '''
     dirname = './test'
     dir_list = os.listdir(dirname)
     images = [cv2.cvtColor(cv2.imread(os.path.join(dirname,img)),cv2.COLOR_BGR2GRAY) for img in dir_list]
-    
-    # crop images
     images = [img[0:img.shape[0]-np.mod(img.shape[0],3),0:img.shape[1]-np.mod(img.shape[1],3)] for img in images]
 
-    # copy images for train and label
-    trains = images.copy()
+    tests = images.copy()
     labels = images.copy()
     
-    # make train images worse
-    trains = [cv2.resize(img, None, fx=1/3., fy=1/3.) for img in trains]
-    trains = [cv2.resize(img, None, fx=3/1., fy=3/1.) for img in trains]
-
-    # reshape
-    trains = [img.reshape(img.shape[0],img.shape[1],1) for img in trains]
+    tests = [cv2.resize(img, None, fx=1/3., fy=1/3.) for img in tests]
+    tests = [cv2.resize(img, None, fx=3/1., fy=3/1.) for img in tests]
+    
+    tests = [img.reshape(img.shape[0],img.shape[1],1) for img in tests]
     labels = [img.reshape(img.shape[0],img.shape[1],1) for img in labels]
 
-    trains = np.array(trains)
+    tests = np.array(tests)
     labels = np.array(labels)
 
-    return trains, labels
+    return tests, labels
 
+def mse(y, t):
+    return np.mean(np.square(y - t))
+
+def psnr(y, t):
+    return 20 * np.log10(255) - 10 * np.log10(mse(y, t))
+
+def ssim(x, y):
+    mu_x = np.mean(x)
+    mu_y = np.mean(y)
+    var_x = np.var(x)
+    var_y = np.var(y)
+    cov = np.mean((x - mu_x) * (y - mu_y))
+    c1 = np.square(0.01 * 255)
+    c2 = np.square(0.03 * 255)
+    return ((2 * mu_x * mu_y + c1) * (2 * cov + c2)) / ((mu_x**2 + mu_y**2 + c1) * (var_x + var_y + c2))
 
 
 
