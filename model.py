009baad9d886e903ca23ec8f23793472732ceb1b
yukia18 <abe@ailab.ics.keio.ac.jp>
2018-07-11 15:25:19 +0900

    tmp

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from keras.optimizers import Adam
 from keras.layers.core import Activation
 
 class SRCNN:
-    def __init__(self, image_size, label_size, c_dim, learning_rate, batch_size, epochs, is_training):
+    def __init__(self, image_size, label_size, c_dim, is_training, learning_rate=1e-4, batch_size=128, epochs=1500):
         self.image_size = image_size
         self.label_size = label_size
         self.c_dim = c_dim
@@ -22,11 +22,11 @@ class SRCNN:
     
     def build_model(self):
         model = Sequential()
-        model.add(Conv2D(64,9,input_shape=(self.image_size,self.image_size,self.c_dim)))
+        model.add(Conv2D(64,9,padding='same',input_shape=(self.image_size,self.image_size,self.c_dim)))
         model.add(Activation('relu'))
-        model.add(Conv2D(32,1))
+        model.add(Conv2D(32,1,padding='same'))
         model.add(Activation('relu'))
-        model.add(Conv2D(self.c_dim,5))
+        model.add(Conv2D(self.c_dim,5,padding='same'))
         optimizer = Adam(lr=self.learning_rate)
         model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['accuracy'])
         return model
@@ -42,20 +42,13 @@ class SRCNN:
         return predicted
     
     def load(self):
-        '''
-        model_filename = 'srcnn_model.json'
-        json_string = open(os.path.join('./',model_filename)).read()
-        model = model_from_json(json_string)
-        optimizer = Adam(lr=self.learning_rate)
-        model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['accuracy'])
-        '''
-        weight_filename = 'srcnn_model_weight.hdf5'
+        weight_filename = 'srcnn_weight.hdf5'
         model = self.build_model()
-        model.load_weights(os.path.join('./',weight_filename))
+        model.load_weights(os.path.join('./model/',weight_filename))
         return model
 
     def save(self):
         json_string = self.model.to_json()
-        open(os.path.join('./','srcnn_model.json'),'w').write(json_string)
-        self.model.save_weights(os.path.join('./','srcnn_model_weight.hdf5'))
+        open(os.path.join('./model/','srcnn_model.json'),'w').write(json_string)
+        self.model.save_weights(os.path.join('./model/','srcnn_weight.hdf5'))
         return json_string
